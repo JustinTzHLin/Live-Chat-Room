72540f2351577d6466620ae04d3c0a48e2d31bc8
Justin Tzu-Hung Lin <hozion612356@gmail.com>
2024-11-19 22:24:46 -0800

    Fixing env variables issue

--- src/app/chatroom/page.tsx
+++ src/app/chatroom/page.tsx
@@ -3,7 +3,9 @@ import ChatRoom from "@/components/chatroom";
 import { io } from "socket.io-client";
 
 const Page = () => {
-  const socket = io("http://localhost:8000");
+  const BACKEND_URL =
+    process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
+  const socket = io(BACKEND_URL);
 
   return <ChatRoom socket={socket} />;
 };
